d49385a1ba9e7e219172295e2db1e12798a48e37
natalie400 <145349624+natalie400@users.noreply.github.com>
2025-04-23 11:17:23 +0300

    added a grading system

--- leap.py
+++ leap.py
@@ -1,9 +1,44 @@
-def is_leap(year: int)-> bool:
 
-  if(year % 4== 0 and year % 100 != 0) or (year % 400 == 0):
-    return True
-  else:
-      return False
+def show_history()->None:
+    print("\n Brief History of python:")
+    print("\n Created in the year 1991:")
+    print("\n py 3 in the year 2008:")
 
-year =int(input("Enter the year: "))
-print(is_leap(year))
+    print("\n*** Program that computes the average score of students:")
+
+def get_names_score() -> tuple[list[str], list[float]]:
+    while True:
+        try:
+            count = int(input("Number of students: "))
+            if count <= 0:
+                print("Please enter a number greater than 0")
+                continue
+                break
+        except ValueError:
+            print("invalid input")
+
+        names_of_students = []
+        scores = []
+
+        print("\n Enter student names and scores:")
+
+        for i in range(count):
+            name = input(f'student {i+1} name: ').strip()
+            if not name:
+                name = f'Student {i+1}'
+                names_of_students.append()
+
+            while True:
+                try:
+                    score = float(input(f"Enter the score for {name}: "))
+                    if 0 <= score <= 100:
+                        scores.append(score)
+                        break
+                    else:
+                        print("score must be between 0 and 100")
+
+                except ValueError:
+                    print("invalid input")
+
+            return names_of_students, scores
+
