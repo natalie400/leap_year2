bf2def27b05d0784b2ca0cbf97394116ac2779e5
natalie400 <145349624+natalie400@users.noreply.github.com>
2025-04-24 21:35:06 +0300

    update

--- leap.py
+++
@@ -1,9 +0,0 @@
-def is_leap(year: int)-> bool:
-
-  if(year % 4== 0 and year % 100 != 0) or (year % 400 == 0):
-    return True
-  else:
-      return False
-
-year =int(input("Enter the year: "))
-print(is_leap(year))
